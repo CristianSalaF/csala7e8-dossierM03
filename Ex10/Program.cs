4a4b34a540a70499b5165cf54b4dc4c2911515c7
CristianSalaF <cristian.sala.7e8@itb.cat>
2025-06-01 01:46:02 +0200

    Add project files.

---
+++ Ex10/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ex10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* int x = 10
+             * string y = "20";
+             * int z = x + y;
+             * Console.WriteLine("El resultat és: " z); */
+
+            int x = 10;
+            int y = 20;
+            int z = x + y;
+            Console.WriteLine("El resultat és: {0}", z);
+            Console.ReadKey();
+        }
+    }
+}
