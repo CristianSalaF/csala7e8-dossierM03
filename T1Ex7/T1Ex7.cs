da6153d3d81b644d317543968b8b0c9d7d9f679a
CristianSalaF <cristian.sala.7e8@itb.cat>
2025-06-01 23:57:45 +0200

    Added T1Ex7, which I've forgotten to add.

---
+++ T1Ex7/T1Ex7.cs
@@ -0,0 +1,52 @@
+﻿namespace T1Ex7
+{
+    internal class T1Ex7
+    {
+        static void Main(string[] args)
+        {
+            const string TxtNumberPrompt = "Introdueix un nombre enter positiu: ";
+            const string TxtDivisors = "Els divisors parells de ";
+            const string TxtNumber = "El nombre ";
+            const string TxtNoDivisors = "No hi ha divisors parells per al nombre ";
+            const string TxtInvalidError = $"Entrada no vàlida. Si us plau, introdueix un enter positiu.";
+            const string TxtPressToExit = "Prem qualsevol tecla per sortir...";
+
+            int number = 0;
+            bool isValidNumber = false;
+
+            while (!isValidNumber )
+            {
+                Console.WriteLine(TxtNumberPrompt);
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out number) && number > 0)
+                {
+                    isValidNumber = true;
+                }
+                else
+                {
+                    Console.WriteLine(TxtInvalidError);
+                }
+            }
+
+            Console.WriteLine($"{TxtDivisors}{number} són:");
+
+            for (int i = 1; i <= number; i++)
+            {
+                if (number % i == 0 && i % 2 == 0)
+                {
+                    Console.WriteLine($"{TxtNumber}{i}");
+                }
+            }
+
+            if (number % 2 != 0)
+            {
+                Console.WriteLine($"{TxtNoDivisors}{number}.");
+            }
+
+            Console.WriteLine(TxtPressToExit);
+            Console.ReadKey();
+
+        }
+    }
+}
