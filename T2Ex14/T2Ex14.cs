32a19650dd9ac884636cfb46309e8c330d632328
CristianSalaF <cristian.sala.7e8@itb.cat>
2025-06-01 23:27:37 +0200

    Added T2Ex14

---
+++ T2Ex14/T2Ex14.cs
@@ -0,0 +1,37 @@
+﻿namespace T2Ex14
+{
+    internal class T2Ex14
+    {
+        static void Main(string[] args)
+        {
+            const string TxtIntro = "Introdueix 5 números: ";
+            const string TxtCurrentNum = "Numero {0}";
+            const string? TxtAllNumbers = "Els números introduïts són:";
+            const string TxtSumNumbers = "La suma de tots els números és: {0}";
+
+            const string TxtPressToExit = "Prem qualsevol tecla per sortir...";
+
+            int[] numeros = new int[5];
+            int suma = 0;
+
+            Console.WriteLine(TxtIntro);
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine(TxtCurrentNum, i+1);
+                numeros[i] = int.Parse(Console.ReadLine());
+                suma += numeros[i];
+            }
+
+            Console.WriteLine(TxtAllNumbers);
+            foreach (int numero in numeros)
+            {
+                Console.WriteLine(numero);
+            }
+
+            Console.WriteLine(TxtSumNumbers, suma);
+
+            Console.WriteLine(TxtPressToExit);
+            Console.ReadKey();
+        }
+    }
+}
