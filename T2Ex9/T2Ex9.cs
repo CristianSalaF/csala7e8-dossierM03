86be56f81d5517f88075a17cb10b8589f667b3fa
CristianSalaF <cristian.sala.7e8@itb.cat>
2025-06-01 18:25:21 +0200

    Added T2Ex9

---
+++ T2Ex9/T2Ex9.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+
+namespace T2Ex9
+{
+    internal class T2Ex9
+    {
+        static void Main(string[] args)
+        {
+            const string TxtPasswordPrompt = "Introdueix una contrasenya: ";
+            const string TxtValidPassword = "La contrasenya és vàlida";
+            const string TxtInvalidPassword = "La contrasenya no és vàlida. Assegura't que té entre 8 i 12 caràcters, inclou almenys una majúscula, una minúscula i un número, i no conté espais.";
+            const string TxtPressToExit = "Prem qualsevol tecla per sortir...";
+
+            string password = "";
+
+            Console.WriteLine(TxtPasswordPrompt);
+            password = Console.ReadLine();
+
+            if (IsValidPassword(password))
+                Console.WriteLine(TxtValidPassword);
+            else
+                Console.WriteLine(TxtInvalidPassword);
+
+            Console.WriteLine(TxtPressToExit);
+            Console.ReadKey();
+        }
+
+        private static bool IsValidPassword(string? password)
+        {
+            const string PasswordPattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[^\s]{8,12}$";
+            return Regex.IsMatch(password, PasswordPattern);
+        }
+    }
+}
