50901876d272937cd1f62479a66930146c1d368a
CristianSalaF <cristian.sala.7e8@itb.cat>
2025-06-02 17:10:04 +0200

    Added T2Ex19

---
+++ T2Ex19/T2Ex19.cs
@@ -0,0 +1,40 @@
+﻿namespace T2Ex19
+{
+    internal class T2Ex19
+    {
+        static void Main(string[] args)
+        {
+            const string TxtNumberPrompt = "Introdueix un nombre enter positiu: ";
+            const string TxtSumOfDigits = "La suma dels dígits del nombre {0} és: {1}";
+            const string TxtInvalidError = "Entrada no vàlida. Si us plau, introdueix un enter positiu.";
+            
+            int number = 0;
+            bool isValidNumber = false;
+            while (!isValidNumber)
+            {
+                Console.WriteLine(TxtNumberPrompt);
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out number) && number >= 0)
+                {
+                    isValidNumber = true;
+                }
+                else
+                {
+                    Console.WriteLine(TxtInvalidError);
+                }
+            }
+
+            int sumOfDigits = 0;
+            int tempNumber = number;
+            while (tempNumber > 0)
+            {
+                sumOfDigits += tempNumber % 10;
+                tempNumber /= 10;
+            }
+            Console.WriteLine(TxtSumOfDigits, number, sumOfDigits);
+
+            Console.WriteLine("Prem qualsevol tecla per sortir...");
+            Console.ReadKey();
+        }
+    }
+}
