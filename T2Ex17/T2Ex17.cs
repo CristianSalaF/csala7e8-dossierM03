1cf0c35192b1eab4245f4b0a694145aedfe29efc
CristianSalaF <cristian.sala.7e8@itb.cat>
2025-06-02 16:56:03 +0200

    Added T2Ex17

---
+++ T2Ex17/T2Ex17.cs
@@ -0,0 +1,77 @@
+﻿namespace T2Ex17
+{
+    internal class T2Ex17
+    {
+        const string TxtNumberPrompt = "Introdueix un nombre enter positiu: ";
+        const string TxtSearchPrompt = "Introdueix un nombre a buscar: ";
+        const string TxtFound = "El nombre {0} es troba a la posició {1} de l'array.";
+        const string TxtNotFound = "El nombre {0} no es troba a l'array.";
+        const string TxtInvalidError = "Entrada no vàlida. Si us plau, introdueix un enter positiu.";
+        const int ArraySize = 10;
+
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[ArraySize];
+            
+            UserNumberArrayInput(numbers);
+            SearchUserNumbers(numbers);
+
+            Console.WriteLine("Prem qualsevol tecla per sortir...");
+            Console.ReadKey();
+        }
+
+        private static void UserNumberArrayInput(int[] numbers)
+        {
+            for (int i = 0; i < ArraySize; i++)
+            {
+                int number;
+                bool isValidNumber = false;
+                while (!isValidNumber)
+                {
+                    Console.WriteLine(TxtNumberPrompt);
+                    string input = Console.ReadLine();
+                    if (int.TryParse(input, out number) && number > 0)
+                    {
+                        numbers[i] = number;
+                        isValidNumber = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine(TxtInvalidError);
+                    }
+                }
+            }
+        }
+
+        private static void SearchUserNumbers(int[] numbers)
+        {
+            bool isValidNumber = false;
+            int searchNumber = 0;
+            
+            while (!isValidNumber)
+            {
+                Console.WriteLine(TxtSearchPrompt);
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out searchNumber) && searchNumber > 0)
+                {
+                    isValidNumber = true;
+                }
+                else
+                {
+                    Console.WriteLine(TxtInvalidError);
+                }
+            }
+
+            bool found = false;
+            for (int i = 0; i < ArraySize && !found; i++)
+            {
+                if (numbers[i] == searchNumber)
+                {
+                    Console.WriteLine(TxtFound, searchNumber, i);
+                    found = true;
+                }
+            }
+            if (!found) Console.WriteLine(TxtNotFound, searchNumber);
+        }
+    }
+}
