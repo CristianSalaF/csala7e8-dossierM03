638a590f0dbd68a5cefee64c43d3e3846875fa4d
CristianSalaF <cristian.sala.7e8@itb.cat>
2025-06-03 20:01:32 +0200

    Added T3, Ex1 and Ex2.

---
+++ T3/Ex1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T3
+{
+    class Ex1
+    {
+        // I've put them in the same file due to it being a short exercise, and many others to add yet.
+        // Otherwise I'd just make a different file for each class.
+        internal static void Exercise()
+        {
+            Pair<DateTime, string> dateStringPair = new Pair<DateTime, string>(DateTime.Now, "Avui és un bon dia");
+            Pair<int, List<string>> intListPair =
+                new Pair<int, List<string>>(3, new List<string> { "Primera", "Segona", "Tercera" });
+
+            var (date, message) = dateStringPair.GetValues();
+            Console.WriteLine($"Data: {date}, Missatge: {message}");
+
+            var (number, list) = intListPair.GetValues();
+            Console.WriteLine($"Número: {number}, Llista: [{string.Join(", ", list)}]");
+        }
+    }
+
+    class Pair<T1, T2>
+    {
+        public T1 First { get; set; }
+        public T2 Second { get; set; }
+
+        public Pair(T1 first, T2 second)
+        {
+            First = first;
+            Second = second;
+        }
+
+        public override string ToString()
+        {
+            return $"({First}, {Second})";
+        }
+
+        public (T1, T2) GetValues()
+        {
+            return (First, Second);
+        }
+    }
+}
---
+++ T3/Ex2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T3
+{
+    class Ex2
+    {
+        internal static void Exercise()
+        {
+            const string TxtPressToExit = "Prem qualsevol tecla per sortir...";
+
+            Hashtable hashtable = new Hashtable
+            {
+                { "nom", "Joan" },
+                { "edat", 30 },
+                { "ciutat", "Barcelona" },
+                { "programador", true }
+            };
+
+            PrintHashtable(hashtable);
+
+            if (!hashtable.ContainsKey("professió")) 
+                hashtable["professió"] = "Desenvolupador";
+
+            if (hashtable.ContainsKey("programador")) 
+                hashtable.Remove("programador");
+
+            Console.WriteLine("\nHashtable actualitzat:");
+            PrintHashtable(hashtable);
+
+            Console.WriteLine(TxtPressToExit);
+            Console.ReadKey();
+        }
+
+        private static void PrintHashtable(Hashtable hashtable)
+        {
+            foreach (DictionaryEntry entry in hashtable)
+            {
+                Console.WriteLine($"Clau: {entry.Key}, Valor: {entry.Value}");
+            }
+        }
+    }
+}
